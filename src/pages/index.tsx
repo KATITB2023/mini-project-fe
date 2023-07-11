17ac558538cdf642994621839303f99d8090c880
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-07-12 02:01:18 +0700

    feat: to-do-list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,169 @@
-import { Button, Center } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import {
+  Heading,
+  Input,
+  Stack,
+  Button,
+  Checkbox,
+  VStack,
+  Text,
+  Box,
+  Flex,
+} from '@chakra-ui/react';
 
-export default function Home() {
-  const [counter, setCounter] = useState(0);
+interface Task {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
-  useEffect(() => {
-    console.log('useEffect kepanggil');
-  }, []);
+export default function Home(): JSX.Element {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [inputValue, setInputValue] = useState('');
+  const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
+  const [editingTaskText, setEditingTaskText] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  // add task
+  const handleAddTask = () => {
+    if (inputValue.trim() !== '') {
+      setTasks([
+        ...tasks,
+        { id: Date.now(), text: inputValue, completed: false },
+      ]);
+      setInputValue('');
+    }
+  };
+
+  // checklist
+  const handleToggleComplete = (id: number) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  // delete task
+  const handleDeleteTask = (id: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  // edit task
+  const handleEditTask = (id: number, text: string) => {
+    setEditingTaskId(id);
+    setEditingTaskText(text);
+  };
+
+  // save edit
+  const handleSaveTask = () => {
+    if (editingTaskId !== null) {
+      const updatedTasks = tasks.map((task) => {
+        if (task.id === editingTaskId) {
+          return { ...task, text: editingTaskText };
+        }
+        return task;
+      });
+      setTasks(updatedTasks);
+      setEditingTaskId(null);
+      setEditingTaskText('');
+    }
+  };
+
+  // cancel edit
+  const handleCancelEdit = () => {
+    setEditingTaskId(null);
+    setEditingTaskText('');
+  };
 
   return (
-    <Center minH='100vh' bgColor={{ base: '#FFFFFF', lg: '#000000' }}>
-      {counter}
-      <Button ml={10} onClick={() => setCounter(counter + 1)}>
-        Increment
-      </Button>
-      <Button ml={10} onClick={() => setCounter(0)}>
-        Reset
-      </Button>
-    </Center>
+    <VStack p={4}>
+      <Heading mb={4}>To-Do List</Heading>
+      <Stack direction="row" spacing={2} mb={4}>
+        <Input
+          placeholder="Masukkin kegiatan kamu!"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Button onClick={handleAddTask}>Add</Button>
+      </Stack>
+      {tasks.length === 0 ? (
+        <Text>Kamu gabut!</Text>
+      ) : (
+        <VStack align="start" spacing={2} w="100%">
+          {tasks.map((task) => (
+            <Box
+              key={task.id}
+              w="100%"
+              _hover={{ bg: 'gray.100' }}
+              p={2}
+              borderRadius="md"
+            >
+              <Flex justify="space-between" align="center">
+                {editingTaskId === task.id ? (
+                  <>
+                    <Input
+                      value={editingTaskText}
+                      onChange={(e) => setEditingTaskText(e.target.value)}
+                    />
+                    <Flex>
+                      <Button
+                        size="xs"
+                        variant="ghost"
+                        colorScheme="green"
+                        onClick={handleSaveTask}
+                      >
+                        Save
+                      </Button>
+                      <Button
+                        size="xs"
+                        variant="ghost"
+                        colorScheme="red"
+                        onClick={handleCancelEdit}
+                      >
+                        Cancel
+                      </Button>
+                    </Flex>
+                  </>
+                ) : (
+                  <>
+                    <Checkbox
+                      isChecked={task.completed}
+                      onChange={() => handleToggleComplete(task.id)}
+                    >
+                      {task.text}
+                    </Checkbox>
+                    <Flex>
+                      <Button
+                        size="xs"
+                        variant="ghost"
+                        colorScheme="yellow"
+                        onClick={() => handleEditTask(task.id, task.text)}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        size="xs"
+                        variant="ghost"
+                        colorScheme="red"
+                        onClick={() => handleDeleteTask(task.id)}
+                      >
+                        Delete
+                      </Button>
+                    </Flex>
+                  </>
+                )}
+              </Flex>
+            </Box>
+          ))}
+        </VStack>
+      )}
+    </VStack>
   );
 }
